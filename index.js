c89012fbcfc8a94ce31f486095748374cb77d733
Dunia Nasr <dunia.nasr777@gmail.com>
2020-07-09 18:10:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,31 @@ const tutorials = [
 const titleCased = () => {
   return tutorials
 }
+const tutorials = [
+  'what does the this keyword mean?',
+  'What is the Contutorialuctor OO pattern?',
+  'implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?'
+];
+
+
+
+const titleCased = () => {
+  const res = []
+
+
+  for(let i = 0; i < tutorials.length; i++){
+    const sent =  tutorials[i].split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+
+    res.push(sent)
+  }
+  return res;
+};
